2a02b99eeb1bb3b1e400f9fec153ae1e1e494b4e
Sivapriya Vellaichamy <77592196+svellaichamy3@users.noreply.github.com>
2022-04-23 16:16:14 -0400

    Update DVA.py

--- DVA.py
+++ DVA.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import streamlit.components.v1 as components
 import numpy as np
 from PIL import Image
 import requests
@@ -75,10 +76,11 @@ st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_h
 #     st.image(images_on_page3, width = 150,caption = movie_names)
 
 
-year = st.slider('Show me the trends of the year', 1940, 2020,step=20)
-a11 = Image.open(str(year)+".png")
-st.image(a11, use_column_width=True, caption = ["Trends"])
+# year = st.slider('Show me the trends of the year', 1940, 2020,step=20)
+# a11 = Image.open(str(year)+".png")
+# st.image(a11, use_column_width=True, caption = ["Trends"])
 
-
+html_temp =""" <div class='tableauPlaceholder' id='viz1650736421918' style='position: relative'><noscript><a href='#'><img alt='Animation ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;KP&#47;KPDTMBDGM&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;KPDTMBDGM' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;KP&#47;KPDTMBDGM&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650736421918');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+components.html(html_temp,width = 1300, height = 700)
 
 
